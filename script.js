f0c4fb165d43289c0a9609339e1fca76a0fb7fa7
ericafk0001 <dxslyofficial@gmail.com>
2024-11-28 20:49:27 -0500

    :3

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+particlesJS.load("particles-js", "particles.json", function () {
+  console.log("particles.js loaded - callback");
+});
+
 const formData = new FormData();
 formData.append("image", imageBlob);
 await fetch("https://api.trace.moe/search", {
