63ccbbdf9b0d280ad8325e3e50dfa9e7abd5ffcc
ericafk0001 <dxslyofficial@gmail.com>
2024-11-28 21:26:33 -0500

    Fix api fetch Started on displaying anime data on webpage

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load("particles-js", "particles.json", function () {
-  console.log("particles.js loaded - callback");
+  console.log("callback - particles.js config loaded");
 });
 
-const formData = new FormData();
-formData.append("image", imageBlob);
-await fetch("https://api.trace.moe/search", {
-  method: "POST",
-  body: formData,
-}).then((e) => e.json());
+document.getElementById("form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const imageInput = document.getElementById("imageInput");
+  const imageBlob = imageInput.files[0]; // Get the selected file
+
+  const resultName = document.getElementById("result-name");
+
+  if (imageBlob) {
+    const formData = new FormData();
+    formData.append("image", imageBlob);
+
+    const response = await fetch("https://api.trace.moe/search", {
+      method: "POST",
+      body: formData,
+    });
+
+    const result = await response.json();
+    console.log(result);
+    resultName.textContent = result;
+  } else {
+    console.error("Error. Did you upload an image?");
+  }
+});
