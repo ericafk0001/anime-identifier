a4eb9623985d1802f4daa6bd383fbbf809d7ed46
Kevin/Ash <life23243@tutanota.com>
2024-11-30 01:56:24 +0000

    update anilist api get anime name

--- script.js
+++ script.js
@@ -9,9 +9,6 @@ document.getElementById("form").addEventListener("submit", async (event) => {
   const imageInput = document.getElementById("imageInput");
   const imageBlob = imageInput.files[0]; // image blob
 
-  const resultName = document.getElementById("result-name");
-  const resultImage = document.getElementById("result-image");
-
   if (imageBlob) {
     const formData = new FormData();
     formData.append("image", imageBlob);
@@ -21,9 +18,15 @@ document.getElementById("form").addEventListener("submit", async (event) => {
       body: formData,
     });
 
+    const resultName = document.getElementById("result-name");
+    const resultImage = document.getElementById("result-image");
+    const resultEpisode = document.getElementById("result-episode");
+    const resultSim = document.getElementById("result-sim");
+
     const result = await response.json();
-    const resultEp = result.result[0];
-    const resultImg = resultEp.image;
+    const resultAnime = result.result[0];
+    const resultSimilarity = result.result[0].similarity;
+    const resultImg = result.result[0].video;
 
     const anilistResponse = await fetch('https://graphql.anilist.co', {
       method: 'POST',
@@ -45,7 +48,7 @@ document.getElementById("form").addEventListener("submit", async (event) => {
           }
         `,
         variables: {
-          id: resultEp.anilist
+          id: resultAnime.anilist
         }
       })
     });
@@ -54,10 +57,15 @@ document.getElementById("form").addEventListener("submit", async (event) => {
     // use english title if available, else use romaji, else use native
     const animeTitle = anilistResult.data.Media.title.english || anilistResult.data.Media.title.romaji || anilistResult.data.Media.title.native;
 
-
     resultName.textContent = `Anime Name: ${animeTitle}`;
+    resultEpisode.textContent = `Episode: ${resultAnime.episode}`;
+    resultSim.textContent = `Similarity: ${(resultSimilarity * 100).toFixed(
+      1
+    )}%`;
     resultImage.src = resultImg;
+    resultImage.style.display = "block";
   } else {
-    console.error("Error. Did you upload an image?");
+    console.log("a problem occured");
+    alert("Error. Did you upload an image?");
   }
 });
