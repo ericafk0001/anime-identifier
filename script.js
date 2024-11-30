8f8b11e9e489f4e4c10cff07afd7b5ad0d884402
Kevin/Ash <life23243@tutanota.com>
2024-11-30 01:05:48 +0000

    Use anilist api to get anime name from id

--- script.js
+++ script.js
@@ -23,10 +23,39 @@ document.getElementById("form").addEventListener("submit", async (event) => {
 
     const result = await response.json();
     const resultEp = result.result[0];
-    const resultImg = result.result[0].image;
+    const resultImg = resultEp.image;
 
-    console.log(result);
-    resultName.textContent = `Anime Name: ${resultEp.filename}`;
+    const anilistResponse = await fetch('https://graphql.anilist.co', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query ($id: Int) {
+            Media (id: $id, type: ANIME) {
+              id
+              title {
+                romaji
+                english
+                native
+              }
+            }
+          }
+        `,
+        variables: {
+          id: resultEp.anilist
+        }
+      })
+    });
+
+    const anilistResult = await anilistResponse.json();
+    // use english title if available, else use romaji, else use native
+    const animeTitle = anilistResult.data.Media.title.english || anilistResult.data.Media.title.romaji || anilistResult.data.Media.title.native;
+
+
+    resultName.textContent = `Anime Name: ${animeTitle}`;
     resultImage.src = resultImg;
   } else {
     console.error("Error. Did you upload an image?");
